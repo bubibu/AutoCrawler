0009faa31687a135f6306786435685f3424b8224
nekonanu <825138000@qq.com>
2013-08-04 14:30:58 +0800

    add the new feature of craw a list of urls in the same time

--- AutoCrawler.py
+++ AutoCrawler.py
@@ -11,6 +11,7 @@ class InternalErrorException(Exception):
 class CookieStr:
     def __init__(self):
         self.cookieDict = dict()
+
     def addCookie(self,key,value):
         self.cookieDict[key] = value
 
@@ -30,9 +31,12 @@ class CookieStr:
 
 class CookieCrawler:
     def __init__(self,url = None,interval = None,cookieDict = None):
+        global timerList
+        timerList = list()
         self.request = None
         self.cookieStr = CookieStr()
         self.interval = None
+        self.timerIndex = -1
         if url is not None:
             self.request = self.__initRequest(url,cookieDict)
         self.interval = interval
@@ -45,8 +49,16 @@ class CookieCrawler:
                 raise InternalErrorException("interval is not set yet!")
         else:
             self.interval = interval
-        res = urllib2.urlopen(self.request)
-        self.__mainLoop(self.request,res,self.cookieStr,self.interval,func)
+
+        self.timerIndex = len(timerList)
+        if isinstance(self.request,dict):
+            res = dict()
+            for url,request in self.request.items():
+                res[url] = urllib2.urlopen(request)
+            self.__mainLoop(self.request,res,self.cookieStr,self.interval,func)
+        else:
+            res = urllib2.urlopen(self.request)
+            self.__mainLoop(self.request,res,self.cookieStr,self.interval,func)
 
     def setUrl(self,url):
         self.request = self.__initRequest(url,self.cookieStr)
@@ -55,23 +67,44 @@ class CookieCrawler:
         if url is None:
             if self.request is None:
                 raise InternalErrorException("url is not set yet!")
-            res = urllib2.urlopen(self.request)
-            return res.read()
         else:
-            self.request = urllib2.Request(url)
-            self.request.add_header('Cookie',self.cookieStr.toStr())
+            if isinstance(url,list):
+                self.request = dict()
+                for urlitem in url:
+                    req = urllib2.Request(urlitem)
+                    req.add_header('Cookie',self.cookieStr.toStr())
+                    self.request[urlitem] = req
+            else:
+                self.request = urllib2.Request(url)
+                self.request.add_header('Cookie',self.cookieStr.toStr())
+        returnDict = dict()
+
+        if isinstance(self.request,dict):
+            for url,request in self.request.items():
+                returnDict[url] = urllib2.urlopen(request).read()
+            return returnDict
+        else:
             res = urllib2.urlopen(self.request)
             return res.read()
 
     def end(self):
+        t = timerList[self.timerIndex]
         t.cancel()
 
     def __initRequest(self,url,cookieDict = None):
         if cookieDict is not None:
             self.cookieStr.replaceAllFromDict(cookieDict)
-        request = urllib2.Request(url)
-        request.add_header('Cookie',self.cookieStr.toStr())
-        return request
+        if isinstance(url,list):
+            requests = dict()
+            for urlitem in url:
+                request = urllib2.Request(urlitem)
+                request.add_header('Cookie',self.cookieStr.toStr())
+                requests[urlitem] = request
+            return requests
+        else:
+            request = urllib2.Request(url)
+            request.add_header('Cookie',self.cookieStr.toStr())
+            return request
 
     def __maintainCookie(self,request,response,cookieStr):
         headers = response.info()
@@ -95,9 +128,22 @@ class CookieCrawler:
         return resDict
 
     def __mainLoop(self,request,response,cookieStr,interval,func):
-        self.__maintainCookie(request,response,cookieStr)
-        res = urllib2.urlopen(request)
-        func(res.read())
-        global t
-        t = threading.Timer(interval,self.__mainLoop,(request,res,cookieStr,interval,func))
-        t.start()
+        if isinstance(request,dict):
+            returnDict = dict()
+            maintainRes = dict()
+            for url,req in request.items():
+                self.__maintainCookie(req,response[url],cookieStr)
+                res = urllib2.urlopen(req)
+                maintainRes[url] = res
+                returnDict[url] = res.read()
+            func(returnDict)
+            t = threading.Timer(interval,self.__mainLoop,(request,maintainRes,cookieStr,interval,func))
+            timerList.append(t)
+            t.start()
+        else:
+            self.__maintainCookie(request,response,cookieStr)
+            res = urllib2.urlopen(request)
+            func(res.read())
+            t = threading.Timer(interval,self.__mainLoop,(request,res,cookieStr,interval,func))
+            timerList.append(t)
+            t.start()
