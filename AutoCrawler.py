4d831d562727472cee5e4a86669f84ae727197c5
nekonanu <825138000@qq.com>
2013-08-12 15:26:51 +0800

    bug fix: the cookie maintain function doesnt trim the new setted cookie string

--- AutoCrawler.py
+++ AutoCrawler.py
@@ -4,6 +4,7 @@ import urllib
 import urllib2
 import threading
 import time
+import sys
 
 class InternalErrorException(Exception):
     def __init__(self,msg):
@@ -14,6 +15,27 @@ class RequestData:
         self.id = id
         self.url = url
         self.data = data
+
+class WorkThread(threading.Thread):
+    def __init__(self,workFunction,sleepTime = None):
+        threading.Thread.__init__(self)
+        self.task = workFunction
+        self.sleepTime = sleepTime
+
+    def run(self):
+        self.running = True
+        self.__work()
+
+    def __work(self):
+        while self.running:
+            self.task()
+            if self.sleepTime is not None:
+                time.sleep(self.sleepTime)
+
+    def stop(self):
+        self.running = False
+
+
 
 class CookieStr:
     def __init__(self,cookieDict = None):
@@ -35,21 +57,32 @@ class CookieStr:
     def toStr(self):
         str = ""
         for key,value in self.cookieDict.items():
-            str += (key+"="+value+";")
+            str += (key.strip(' ')+"="+value.strip(' ')+";")
         return str
 
 class CookieCrawler:
     def __init__(self,requestData,interval = None,cookieDict = None,option = None):
+        # const fields
+        self.checkInterval = 3
+        self.breakInterval = 3
+
         # option definition
         self.showLog = False
+        self.debug = False
         if option is not None:
             if option.has_key('showLog'):
                 self.showLog = option['showLog']
+            if option.has_key('debug'):
+                self.debug = option['debug']
+
+        if self.debug:
+            self.errorFile = file(str(time.time()) + "debug.log","w")
         # initial fields
         self.cookieStr = CookieStr()
         self.interval = interval
         self.request = self.__initRequest(requestData,cookieDict)
         self.interval = interval
+        self.lastCrawlTime = time.time()
 
     def start(self,func,interval = None):
         self.handler = func
@@ -62,8 +95,29 @@ class CookieCrawler:
         else:
             self.interval = interval
 
-        th = threading.Thread(target=self.__mainLoop)
-        th.start()
+        self.thread = WorkThread(self.__mainLoop,self.interval)
+        self.thread.start()
+
+        checkThread = WorkThread(self.__checkStatus)
+        checkThread.start()
+
+    def __checkStatus(self):
+        # every x intervals where x is defined by the checkInterval
+        # the program will check the crawler status to make sure the crawler work normally
+        while self.thread.isAlive():
+            self.thread.join(self.checkInterval * self.interval)
+            if self.showLog:
+                print "check status!"
+                print time.time()
+                print self.lastCrawlTime
+            if time.time() - self.lastCrawlTime >= self.breakInterval * self.interval:
+                # the program has something wrong
+                # stop the current crawl process and restart
+                print "join!"
+                self.thread.stop()
+                self.thread.join()
+                print "restart the thread"
+                self.thread.start()
 
     def setUrl(self,url):
         self.request = self.__initRequest(url,self.cookieStr)
@@ -104,6 +158,10 @@ class CookieCrawler:
             result.close()
             return content
 
+    def __log(self,msg):
+        self.errorFile.write(msg + "\n")
+        self.errorFile.flush()
+
     def __initRequest(self,requestData,cookieDict = None):
         if cookieDict is not None:
             self.cookieStr.replaceAllFromDict(cookieDict)
@@ -123,14 +181,9 @@ class CookieCrawler:
         if 'Set-Cookie' in headers:
             ckDict = self.__getCookieFromHeadStr(headers['Set-Cookie'])
             for key,value in ckDict.items():
-                cookieStr.replaceCookie(key,value)
-
-            request.add_header('Cookie',cookieStr.toStr())
+                cookieStr.replaceCookie(key.strip(' '),value.strip(' '))
             for id,req in self.request.items():
-                if req.get_full_url() == request.get_full_url():
-                    if self.showLog:
-                        print "urlID:" + str(id) + " add cookie"
-                    req.add_header('Cookie',cookieStr.toStr())
+                req.add_header('Cookie',cookieStr.toStr())
             if self.showLog:
                 print "receive new cookies: " + str(ckDict)
                 print "cookieHeader:" + self.cookieStr.toStr()
@@ -147,29 +200,40 @@ class CookieCrawler:
         return resDict
 
     def __mainLoop(self):
-        while True:
-            start = time.time()
-            returnDict = dict()
-            maintainRes = dict()
-            for id,req in self.request.items():
-                maintainRes[id] = self.__crawl(req)
-                self.__maintainCookie(req,maintainRes[id],self.cookieStr)
+        start = time.time()
+        returnDict = dict()
+        maintainRes = dict()
+        for id,req in self.request.items():
+            maintainRes[id] = self.__crawl(req)
+            self.__maintainCookie(req,maintainRes[id],self.cookieStr)
 
-            end = time.time()
-            cost = end - start
+        end = time.time()
+        cost = end - start
+        if self.showLog:
+            print "complete!"
+            print "time cost:" + str(cost) + "s"
+        if self.interval is not None and self.interval <= cost:
+            self.interval = cost*1.5
             if self.showLog:
-                print "complete!"
-                print "time cost:" + str(cost) + "s"
-            if self.interval is not None and self.interval <= cost:
-                self.interval = cost*1.5
-                if self.showLog:
-                    print "interval is less than crawling time!"
-                    print "adjust interval to " + str(self.interval)
+                print "interval is less than crawling time!"
+                print "adjust interval to " + str(self.interval)
 
-            for id,req in maintainRes.items():
-                returnDict[id] = req.read()
-            self.handler(returnDict)
-            time.sleep(self.interval)
+        for id,req in maintainRes.items():
+            returnDict[id] = req.read()
+        self.lastCrawlTime = time.time()
+        self.handler(returnDict)
+        if self.debug:
+            self.__showStatus()
+
+    def __showStatus(self):
+        self.__log("crawler status[" + str(time.strftime("%Y-%m-%d %X",time.localtime())) + "]:")
+        self.__log("craw interval:" + str(self.interval))
+        self.__log("craw cookie:" + self.cookieStr.toStr())
+        self.__log("craw request:" + str(self.request))
+        self.__log("craw thread alive?:" + str(self.thread.isAlive()))
+        self.__log("last craw time:" + str(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(self.lastCrawlTime))))
+        if time.time() - self.lastCrawlTime >= self.breakInterval * self.interval:
+            self.__log("crawl breakdown!")
 
     # function  use urllib2 to open the request, return the response
     # para      request:single request object, not list
